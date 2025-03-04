9b620f56e3b52742fe96e525c00352152de3fb99
Seenery27 <36343880+Seenery27@users.noreply.github.com>
2025-03-04 12:54:07 +0100

    Update tlistaposicion.cc

---
+++ tlistaposicion.cc
@@ -0,0 +1,47 @@
+#include "tlistaposicion.h"
+
+TListaPosicion::TListaPosicion() {
+    pos = nullptr;
+}
+
+TListaPosicion::TListaPosicion(const TListaPosicion& o) {
+    pos = o.pos;
+}
+
+TListaPosicion::~TListaPosicion() {pos = nullptr;}
+
+TListaPosicion&
+TListaPosicion::operator=(const TListaPosicion& o2) {
+    (*this).~TListaPosicion();
+    pos = o2.pos;
+
+    return *this;
+}
+
+bool
+TListaPosicion::operator==(const TListaPosicion &o) {
+    return pos == o.pos;
+}
+
+TListaPosicion
+TListaPosicion::Siguente() {
+    TListaPosicion nextPos;
+    if (pos && pos->siguente) {
+        nextPos.pos = pos->siguente;
+    }
+    return nextPos;
+}
+
+
+TListaPosicion
+TListaPosicion::Anterior() {
+    TListaPosicion lastPos;
+    if (pos && pos->anterior) {
+        lastPos.pos = pos->anterior;
+    }
+    return lastPos;
+}
+
+bool TListaPosicion::EsVacia() {
+    return pos == nullptr;
+}
