e0b1d287ba7a6c0fed17e75a6e160aecc21a3dcb
Seenery27 <36343880+Seenery27@users.noreply.github.com>
2025-03-04 12:53:50 +0100

    Update tlistaposicion.h

--- tlistaposicion.h
+++ tlistaposicion.h
@@ -1,17 +1,25 @@
 #include <iostream>
 #include "tlistanodo.h"
+#ifndef __TLISTAPOSICION__
+#define __TLISTAPOSICION__
+
 using namespace std;
 
 class TListaPosicion {
+    friend class TListaPoro;
+    friend class TListaNodo;
+
     private:
         TListaNodo *pos;
     public:
         TListaPosicion();
-        TListaPosicion(TListaPosicion &);
+        TListaPosicion(const TListaPosicion &);
         ~TListaPosicion();
-        TListaPosicion& operator=(TListaPosicion &);
-        bool operator==(TListaPosicion &);
+        TListaPosicion& operator=(const TListaPosicion &);
+        bool operator==(const TListaPosicion &);
         TListaPosicion Anterior();
         TListaPosicion Siguente();
         bool EsVacia();
 };
+
+#endif
